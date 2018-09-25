4ba93260db9becb6e74301aa3d771cde5bef0c27
sara <sarneijim@gmail.com>
2018-09-25 00:00:36 -0400

    dubi

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html");
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TOKEN'));
+});
+?>
